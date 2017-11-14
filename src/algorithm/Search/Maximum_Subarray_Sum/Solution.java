deb56538a9db039742c81da024d87027925dfef1
siehuai <edricgan.44@gmail.com>
2017-11-14 12:14:15 +0800

    Maximum Subarray Sum Solution

--- src/algorithm/Search/Maximum_Subarray_Sum/Solution.java
+++ src/algorithm/Search/Maximum_Subarray_Sum/Solution.java
@@ -1,44 +1,13 @@
 package algorithm.Search.Maximum_Subarray_Sum;
 
-import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
+import java.util.Scanner;
+import java.util.TreeSet;
 
+/*The idea of this is very tricky, it requires some mathematical understanding
+  but is nevertheless an interesting question. Links to reference:
+  https://www.quora.com/What-is-the-logic-used-in-the-HackerRank-Maximise-Sum-problem
+ */
 public class Solution {
-
-    static long maximumSum(long[] a, long m) {
-        long max = Long.MIN_VALUE;
-        for (int group = 0; group < a.length; group++) {
-            for (int start = 0; start < a.length; start++) {
-                long result = recur(start, group, a, m);
-                max = Math.max(max, result);
-                if (result == m - 1) {
-                    return m - 1;
-                }
-            }
-        }
-        return max;
-    }
-
-    static long recur(int start, int group, long[] arr, long m) {
-        int k = start + 1;
-        long group_sum = 0;
-        long max = Long.MIN_VALUE;
-        while (k + group <= arr.length) {
-            for (int j = k; j < k + group; j++) {
-                group_sum += arr[j];
-            }
-            group_sum += arr[start];
-            group_sum = group_sum % m;
-            max = Math.max(group_sum, max);
-            k++;
-            group_sum = 0;
-        }
-        return max;
-    }
-
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int q = in.nextInt();
@@ -49,9 +18,37 @@ public class Solution {
             for (int a_i = 0; a_i < n; a_i++) {
                 a[a_i] = in.nextLong();
             }
-            long result = maximumSum(a, m);
-            System.out.println(result);
+            System.out.println(solve(a, m));
         }
         in.close();
+    }
+
+    public static long solve(long[] A, long m) {
+        int length = A.length;
+        long[] prefix_sum = new long[length];
+        long running_sum = 0;
+        for (int i = 0; i < length; i++) {
+            running_sum += A[i];
+            prefix_sum[i] = running_sum % m;
+        }
+        TreeSet<Long> treeSet = new TreeSet<>();
+        long max = Long.MIN_VALUE;
+        //Key idea is why are we looking for the smallest bigger number. Please look at the explaination below:
+        for (int i = 0; i < length; i++) {
+            Long higher = treeSet.higher(prefix_sum[i]);
+            if (higher == null) {
+                higher = 0L;
+            }
+            /* prefix[i] - higher + m, if (prefix[i] - higher) is positive, after
+               adding with m and modulus with m, it will be prefix[i] - higher, which is
+               smaller than prefix[i]. If (prefix[i]-higher) is negative, after
+               adding with m and modulus with m, you have a better chance in getting a bigger number
+               but you only want this negative number to be so small that it only minus a little bit from
+               m, so that you can get the largest value after modulus with m !
+             */
+            long result = (prefix_sum[i] - higher + m) % m;
+            max = Math.max(result, max);
+        }
+        return max;
     }
 }
